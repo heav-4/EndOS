5cbbf3d07fdd4bc44958162b16ada4035ccaa7a5
heav-4 <46372165+heav-4@users.noreply.github.com>
2020-06-12 23:35:32 +0100

    Update endos.lua
    has stuff

--- endos.lua
+++ endos.lua
@@ -1,5 +1,7 @@
 if shell.getRunningProgram() ~= "startup.lua" then
     fs.move("startup.lua","startup-"..math.random(34953845,345839583498)..".lua")
-    shell.run("wget [PLACEHOLDER] startup.lua")
+    shell.run("/rom/programs/http/wget.lua https://raw.githubusercontent.com/heav-4/EndOS/master/endos.lua startup.lua")
     os.reboot()
 end
+
+print("Hi!")
